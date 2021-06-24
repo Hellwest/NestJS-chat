4ccaf81cf7eebd174dd7cd2a8d37b1c023d4ca9b
Vladislav Nikitin <dushekrad@gmail.com>
2021-06-24 18:14:15 +0400

    feat(api): add config library #23

---
+++ packages/api/src/config/graphql.config.ts
@@ -0,0 +1,45 @@
+import config = require("config")
+import * as depthLimit from "graphql-depth-limit"
+import { DateScalarMode, GqlModuleOptions } from "@nestjs/graphql"
+import { getBoolean } from "./get-boolean"
+
+interface GQLConfig {
+  debug: boolean
+  playground: boolean
+  introspection: boolean
+  autoSchemaFile: string | boolean
+  dateScalarMode: DateScalarMode
+  depthLimit: number
+}
+
+const configFromFile = config.get<GQLConfig>("graphql")
+
+export const graphQLConfig: GqlModuleOptions = {
+  validationRules: [
+    depthLimit(process.env.GRAPHQL_DEPTH_LIMIT || configFromFile.depthLimit),
+  ],
+  debug: getBoolean(process.env.GRAPHQL_DEBUG, configFromFile.debug),
+  playground: getBoolean(
+    process.env.GRAPHQL_PLAYGROUND,
+    configFromFile.playground,
+  ),
+  introspection: getBoolean(
+    process.env.GRAPHQL_INTROSPECTION,
+    configFromFile.introspection,
+  ),
+  autoSchemaFile:
+    process.env.GRAPHQL_AUTH_SCHEMA_FILE || configFromFile.autoSchemaFile,
+  buildSchemaOptions: {
+    dateScalarMode:
+      (process.env.GRAPHQL_DATE_SCALAR_MODE as DateScalarMode) ||
+      configFromFile.dateScalarMode,
+  },
+  installSubscriptionHandlers: true,
+  context: ({ req, connection }: any): Record<string, unknown> => ({
+    req,
+    connection,
+  }),
+  subscriptions: {
+    path: "/subscriptions",
+  },
+}
