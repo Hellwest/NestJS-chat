0ca5e07e41639d19bd8f09426d9e1dcc77a4fea9
Vladislav Nikitin <dushekrad@gmail.com>
2021-06-25 13:33:14 +0400

    chore(api): remove redundant files #15

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from "@nestjs/common"
 import { GraphQLModule } from "@nestjs/graphql"
 
 import { AppController } from "./app.controller"
-import { AppService } from "./app.service"
 import { graphQLConfig } from "./config/graphql.config"
 import { AuthModule } from "./auth/auth.module"
 import { UsersModule } from "./users/users.module"
@@ -10,6 +9,5 @@ import { UsersModule } from "./users/users.module"
 @Module({
   imports: [GraphQLModule.forRoot(graphQLConfig), AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
