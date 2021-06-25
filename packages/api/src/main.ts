b30e486bcc6af0f2268a9417a33513baae2d1872
Vladislav Nikitin <dushekrad@gmail.com>
2021-06-25 12:21:37 +0400

    feat(api): add bootstrap logger

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,9 +1,21 @@
+import { Logger } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 
 import { AppModule } from "./app.module"
+import { serverConfig } from "./config/server.config"
 
 async function bootstrap() {
+  const logger = new Logger("Bootstrap")
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  try {
+    await app.listen(serverConfig.port)
+  } catch (error) {
+    logger.error(`Application starting failed: ${JSON.stringify(error)}`)
+    throw error
+  }
+
+  logger.log(`Application is listening on port: ${serverConfig.port}`)
 }
+
 bootstrap()
